ca5976c034a3fe652179860a74d9b763cedc4342
ckempema <calderkempema@gmail.com>
2018-11-06 10:50:19 -0500

    Added Production API
    All team members agreed to push direct to master.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://limitless-inlet-83543.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
