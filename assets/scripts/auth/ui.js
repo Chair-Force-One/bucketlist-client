8bf70cc47474b803d207688f1912753783e2e096
ckempema <calderkempema@gmail.com>
2018-11-06 09:49:29 -0500

    Linked user auth
    Calder/Charles
    
    Linked user sign in/up change password and sign out buttons, working 
    with base level server

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,55 +1,49 @@
 'use strict'
 
 const store = require('../store.js')
-const state = require('../states.js')
 
 const signUpSuccess = (response) => {
-  $('#auth_messages').html('')
+  $('#user-messages').html('')
   const userHTML = (`
     <h4> New User: ${response.user.email}</h4>
     `)
-  $('#auth_messages').html(userHTML)
+  $('#user-message').html(userHTML)
   $('#sign-up-form').trigger('reset')
-  $('#sign-up-box').hide()
-  $('#sign-in-box').show()
 }
 
 const signInSuccess = (response) => {
-  $('#auth_messages').html('')
+  $('#user-message').html('')
   // TODO: Remove unnecessary data from output
   const userHTML = (`
     <h5>${response.user.email}<h5>
     `)
-  $('#user-message').append(userHTML)
+  $('#user-message').html(userHTML)
   store.user = response.user
-  state.signedIn()
   $('#sign-in-form').trigger('reset')
 }
 
 const changePasswordSuccess = (response) => {
-  $('#auth_messages').html('')
+  $('#user-message').html('')
   const outputHTML = (`
     <h6>Password Changed!</h6>
     `)
-  $('#auth_messages').append(outputHTML)
-  state.signedIn()
+  $('#user-message').html(outputHTML)
   $('#change-password-form').trigger('reset')
 }
 
 const signOutSuccess = (response) => {
-  $('#auth_messages').html(`<h5> Signed Out </h5>`)
-  state.signedOut()
+  $('#user-message').html(`<h5> Signed Out </h5>`)
   // store.user = null // remove all stored data on logout
   // store.currentGame = null
 }
 
 // OPTIMIZE: Create failure functions for each possible state rather than a blanket case
 const failure = (response) => {
-  $('#auth_messages').html('')
+  $('#user-message').html('')
   const responseHTML = (`
     <h3>ERROR: Failed to authenticate with server</h3>
     `)
-  $('#auth_messages').append(responseHTML)
+  $('#user-message').html(responseHTML)
 }
 
 module.exports = {
